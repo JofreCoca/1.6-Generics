47c7c9e9a723cb8bc3f0596927bb609cba6f5968
JofreCoca <jofrecoca@gmail.com>
2024-12-04 10:23:16 +0100

    Start n1 e1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Generics\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        NoGenericMethods example1 = new NoGenericMethods("One", 123, true);
+        System.out.println("Example 1: " + example1);
+
+
+        NoGenericMethods example2 = new NoGenericMethods(3.14, "Two", false);
+        System.out.println("Example 2: " + example2);
+
+        example1.setObject1(123);
+        example1.setObject2(false);
+        example1.setObject3("Nou valor");
+        System.out.println("Example 3 després de modificar: " + example1);
+
+        // Recuperar objectes específics
+        System.out.println("Objecte 1: " + example1.getObject1());
+        System.out.println("Objecte 2: " + example1.getObject2());
+        System.out.println("Objecte 3: " + example1.getObject3());
     }
 }
